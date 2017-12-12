6555c6bb5ff8ad54c80b00b4bd0cb4d50d315dd0
Polina Manevich <polina.ma92@gmail.com>
2017-12-12 21:14:36 +0200

    changed CmakeList (name of project) and adde basic file reading in main c

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    printf("Opening File ...\n");
+    FILE *fp;
+    char buff[255];
+
+    fp = fopen("C:\\Polina\\Technion\\Semester7\\Dedup Project\\Project_Files\\DeduplicationProject\\input_example.txt" , "r");
+    if(fp ==NULL){
+        printf("Can't open input file\n");
+        return 0;
+    }
+
+    printf("The contents of the file are:");
+    for (int i = 0; i < 10 ; ++i) {
+        fgets(buff, 255 , fp);
+        printf("%s\n" , buff);
+    }
+
+    printf("Finished\n");
+    fclose(fp);
     return 0;
 }
