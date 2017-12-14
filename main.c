4882ec77341296f25b945394699323d5824e7730
Polina Manevich <polina.ma92@gmail.com>
2017-12-14 20:25:38 +0200

    updated file parsing

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 
@@ -29,46 +30,80 @@ int main() {
     /* ------------------------------------------ Get File Names To Process ----------------------------------------- */
     /* -------------------------------------------------------------------------------------------------------------- */
     /* ------------------------------------------------ File Opening ------------------------------------------------ */
-    printf("Opening File ...\n");
+    printf(" --- Opening File --- \n");
     //input_file = fopen("//home//mihuahams//project_files//input_example.txt" , "r");
     input_file = fopen("C:\\Polina\\Technion\\Semester7\\Dedup Project\\Project_Files\\DeduplicationProject\\input_example.txt" , "r");
     res_file = fopen("C:\\Polina\\Technion\\Semester7\\Dedup Project\\Project_Files\\DeduplicationProject\\res_file.txt" , "w");
     if(input_file == NULL){ //check the file was opened successfully - if not terminate
-        printf("Can't open input file\n");
+        printf(" --- Can't open input file --- \n");
         return 0;
     }
     printf("---------------------------------------------------------------------------------------------------------");
     /* ------------------------------------------------ File Opening ------------------------------------------------ */
     /* -------------------------------------------------------------------------------------------------------------- */
     /* ------------------------------------------------ File Reading ------------------------------------------------ */
-    printf("Start Reading the file ... \n");
+
+    int block_line_count = 0;
+    char dir_name_hash[11];
+    int namespace_depth;
+    int file_size;
+    int file_attribute;
+    char file_id[16];
+
+    printf(" --- Start Reading the file --- \n");
     do{ //skip till the first empty line - over the file system description
         fgets(buff, 255 , input_file);
     } while(strlen(buff) > 1);
-    printf("Skipped over the file-system data block");
+    printf(" --- Skipped over the file-system data block - lets start reading the real data --- \n");
 
-
-    printf("---> now lets start Reading the real data ...\n");
-    while(!feof(input_file)){//Read File till the end
+    while(!feof(input_file)){//Read File till the end - parse each block and add it to the corresponding structure
         fgets(buff, 255 , input_file);
-        if(strcmp(buff , "LOGCOMPLETE\n") == 0){ //You have reached the end of the file, nothing more to read
-            printf("%d - We have reached the end of the file\n", strcmp(buff , "LOGCOMPLETE"));
+        block_line_count++;
+        if(strcmp(buff , "LOGCOMPLETE\n") == 0){ //Check if we have reached the end of the file, nothing more to read
             fileEnd = true;
             fgets(buff, 255 , input_file); // read the last line before EOF
         }
-        if(fileEnd == false){
-            while (strlen(buff) > 1 && !feof(input_file)){
+        if(fileEnd == false){ // We havent seen the LOGCOMPLETE line yet
+            while (strlen(buff) > 1 && !feof(input_file)){ // Check if we haven't reached the end of the current input block
                 fputs(buff , res_file);
-                //printf("%s  -> %d \n" , buff , strlen(buff));
-                fgets(buff, 255 , input_file);
+                switch(block_line_count){
+                    case 1: //save the directory name
+                        strncpy(dir_name_hash , buff , 10); //only first 10 digits depict the hashed directory name
+                        dir_name_hash[10] = '\0';
+                        fprintf(res_file , "-->File name is: %s \n" , dir_name_hash);
+                        break;
+                    case 4: //save the namespace depth
+                        namespace_depth = atoi(buff);
+                        fprintf(res_file , "--> Namespace depth is : %d \n" , namespace_depth);
+                        break;
+                    case 5: // save the file size
+                        file_size = atoi(buff);
+                        fprintf(res_file , "--> File size is : %d \n" , file_size);
+                        break;
+                    case 6: //save the file attributes value
+                        file_attribute = atoi(buff);
+                        fprintf(res_file , "--> File attribute is : %d \n" , file_attribute);
+                        break;
+                    case 7: //save the hashed file id
+                        strncpy(file_id , buff , 15); //only first 10 digits depict the hashed directory name
+                        file_id[15] = '\0';
+                        fprintf(res_file , "-->File id is: %s \n" , file_id);
+                        break;
+                    default:
+                        break;
+                }
+
+                fgets(buff, 255 , input_file); //read next line in current block
+                block_line_count++;
             }
-            if(!feof(input_file)){
-                fputs("-----------> finished Reading a block\n\n" , res_file);
+            if(!feof(input_file)){ // !!! WE HAVE REACHED THE END OF THE CURRENT INPUT BLOCK !!!
+                fputs("-------> finished Reading a block\n\n" , res_file);
+                block_line_count = 0; //zero the line count for the next block
             }
         }
     }
-    printf("---------------------------------------------------------------------------------------------------------");
-    printf("Finished reading the input file - Now lets start processing ...\n");
+
+    printf(" --- Finished reading the input file - Now lets start processing ---\n");
     /* ------------------------------------------------ File Reading ------------------------------------------------ */
     /* -------------------------------------------------------------------------------------------------------------- */
     /* ------------------------------------------------ File Closing ------------------------------------------------ */
