cd6b07f03a616fa0e73ee25a0b6ff6de1adb69da
Polina Manevich <polina.ma92@gmail.com>
2018-05-04 14:38:33 +0300

    4.5.18 - 14:38

--- memory_pool.c
+++ memory_pool.c
@@ -4,40 +4,42 @@
 
 #include "memory_pool.h"
 
-Dedup_Error_Val memory_pool_init(PMemory_pool pool)
+// Creates the struct of a Memory Pool that contains 4 values
+void* memory_pool_init(PMemory_pool pool)
 {
-	memset(pool, 0, sizeof(Memory_pool));
-	return SUCCESS;
+	return memset(pool, 0, sizeof(Memory_pool));
 }
 
-Dedup_Error_Val memory_pool_alloc(PMemory_pool pool, uint32 size, uint32 **res)
+void* memory_pool_alloc(PMemory_pool pool, uint32_t size)
 {
-	/* size is in bytes, but we will allocate memory in sizeof(uint32) chunks so we will need to calculate how much
-		to allocate */
-	uint32 size_in_uint32_uints = (size / sizeof(uint32)); 
-	uint32 size_of_uint32_to_alloc = (size % sizeof(uint32)) ? size_in_uint32_uints  + 1 : size_in_uint32_uints;
-	
+	void* res;
+	/* size is in bytes, but we will allocate memory in sizeof(uint32)
+	 * chunks so we will need to calculate how much to allocate */
+	uint32_t size_in_uint32_uints = (size / sizeof(uint32_t));
+    uint32_t size_of_uint32_to_alloc = (size % sizeof(uint32_t)) ? size_in_uint32_uints  + 1 : size_in_uint32_uints;
+
 	PMemory_pool pool_to_alloc_from = pool;
-	uint32 pool_to_alloc_from_index = pool->next_free_pool_index;
-	uint32 i;
+    uint32_t pool_to_alloc_from_index = pool->next_free_pool_index;
+    uint32_t i;
 
 	/* we cannot allocate memory larger than the size of an empty pool */
 	assert(size_of_uint32_to_alloc < POOL_INITIAL_SIZE);
 
 	/* find the pool node that has empty memory in it */
-	for (i = 0; i < pool_to_alloc_from_index; i++)
-	{
+	//Go to Last Pool (Which is supposed to have empty place in it)
+	for (i = 0; i < pool_to_alloc_from_index; i++) {
 		pool_to_alloc_from = pool_to_alloc_from->next_pool;
 	}
 
-	/* check that the pool has enough memory to allocate the requested size, if not, allocate
-		a new pool node */
+	/* check that the pool has enough memory to allocate the requested size, if not,
+	 * allocate a new pool node */
 	if (POOL_INITIAL_SIZE <= (pool->next_free_index + size_of_uint32_to_alloc))
 	{
-		pool_to_alloc_from->next_pool = calloc(1,sizeof(Memory_pool));
+		//Allocating new pool node
+		pool_to_alloc_from->next_pool = calloc(1 , sizeof(Memory_pool));
 		if (!pool_to_alloc_from->next_pool)
 		{
-			return ALLOCATION_FAILURE;
+			return NULL;
 		}
 		pool->next_free_index = 0;
 		pool->next_free_pool_index++;
@@ -45,13 +47,12 @@ Dedup_Error_Val memory_pool_alloc(PMemory_pool pool, uint32 size, uint32 **res)
 	}
 
 	/* place the address of the memory in res and update the free index */
-	*res = &(pool_to_alloc_from->arr[pool->next_free_index]);
+	res = &(pool_to_alloc_from->arr[pool->next_free_index]);
 	pool->next_free_index += size_of_uint32_to_alloc;
-
-	return SUCCESS;
+	return res;
 }
 
-Dedup_Error_Val memory_pool_destroy(PMemory_pool pool)
+void memory_pool_destroy(PMemory_pool pool)
 {
 	PMemory_pool next_pool = NULL;
 	PMemory_pool pool_to_free = pool->next_pool;
@@ -64,5 +65,5 @@ Dedup_Error_Val memory_pool_destroy(PMemory_pool pool)
 		pool_to_free = next_pool;
 	}
 
-	return SUCCESS;
+	return ;
 }
